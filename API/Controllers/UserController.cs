fa4a608908fa0f0d17279ff92de55d7b8fcdbb01
Yelena Gouralnik <ygouralnik@tms.ninja>
2021-11-20 22:30:06 -0700

    Initial Commit

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UserController(DataContext context)
+        {
+            _context = context;
+        }
+        //api/user
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> Get()
+        {
+            var users = await _context.Users.ToListAsync();
+            return users;
+        }
+        //api/user/1
+         [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> Get(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+### Project Setup
+* using dotnet ef cli
+* dotnet ef migrations add InitialCreate -o Data/Migrations
+* dotnet ef database update
+* dotnet dev-certs https --trust
