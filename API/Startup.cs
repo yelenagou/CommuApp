65a5cdaf9390dcda8fdfcb738bfab1cbede63e18
Yelenago <lenags@gmail.com>
2021-12-17 16:17:18 -0700

    new commit

--- API/Startup.cs
+++ API/Startup.cs
@@ -42,6 +42,7 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -57,6 +58,9 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseCors(
+                x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200","https://localhost:4200")
+            );
 
             app.UseAuthorization();
 
--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 * dotnet ef migrations add InitialCreate -o Data/Migrations
 * dotnet ef database update
 * dotnet dev-certs https --trust
+* npm install -g @angular/cli@12
+* ng new client --strict false
+* ng serve
+* ng add ngx-bootstrap
